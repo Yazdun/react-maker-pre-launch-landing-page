037fa76236b4e9df2bf53ef9614b7631900a641f
Yazdun <yazduntube@gmail.com>
2022-09-08 19:00:38 +0430

    commit

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,7 @@ import { Logo } from '../'
 
 export const Navbar = () => {
   return (
-    <nav className="container absolute top-0 flex justify-center p-5 m-auto md:justify-start md:static">
+    <nav className="container absolute top-0 left-0 right-0 flex justify-center p-5 m-auto md:justify-start ">
       <Logo />
     </nav>
   )
